b26803660672e7919115e620582b1dd53bf8ee7d
seishu-wataru <seishu-wataru@F-Ebisu-0051.local>
2019-01-17 16:22:47 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 var AWS = require('aws-sdk');
 
 AWS.config.update({
-    // Dynamoのリージョンに合わせて書き換える
-    region: "us-west-2"
+  region: "us-west-2"
 });
 
 var dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
     // 現在時刻の取得
-    var dt    = new Date();
+    var date   = new Date();
     
     // 日本の時間に修正
-    dt.setTime(dt.getTime() + 32400000); // 1000 * 60 * 60 * 9(hour)
+    date.setTime(date.getTime() + 32400000); // 1000 * 60 * 60 * 9(hour)
     
     // 日付を数字として取り出す
-    var year  = dt.getFullYear();
-    var month = dt.getMonth()+1;
-    var day   = dt.getDate();
-    var hour  = dt.getHours();
-    var min   = dt.getMinutes()
+    var year  = date.getFullYear();
+    var month = date.getMonth()+1;
+    var day   = date.getDate();
+    var hour  = date.getHours();
+    var min   = date.getMinutes()
     
     // 値が1桁であれば '0'を追加 
     if (month < 10) {
@@ -39,19 +38,18 @@ exports.handler = (event, context, callback) => {
     }
     
     // 出力
-    var Date_now = year + '/' + month  + '/' + day + ' ' + hour  + ':'+ min;
-    console.log('Received event:' + Date_now);
+    var date_now = year + '/' + month  + '/' + day + ' ' + hour  + ':'+ min;
+    console.log('Received event:' + date_now);
 
     console.log("event:", event);
     dynamo.put({
         "TableName": "Complaints",
         "Item": {
-            "Complaints": event.complaints,
-            "PostedDate": Date_now
+            "text": event.text,
+            "created_at": date_now
         }
     }, function( err, data ) {
         console.log("dynamo_err:", err);
         context.done(null, data);
     });
 };
-
